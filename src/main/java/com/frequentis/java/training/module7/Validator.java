aa96a128985b322f10113163336d2ec6b4fdcd47
Artudean Alexandra <Alexandra-Ana-Maria.ARTUDEAN@frequentis.com>
2019-08-26 16:26:27 +0300

    baza16

--- src/main/java/com/frequentis/java/training/module7/Validator.java
+++ src/main/java/com/frequentis/java/training/module7/Validator.java
@@ -1,5 +1,6 @@
 package com.frequentis.java.training.module7;
 
+import com.frequentis.java.training.module7.exception.ConversionException;
 import com.frequentis.java.training.module7.exception.InvalidCombinedCaseDeliverable;
 import com.frequentis.java.training.module7.exception.InvalidLowerCaseDeliverable;
 import com.frequentis.java.training.module7.exception.InvalidUpperCaseDeliverable;
@@ -35,5 +36,30 @@ public class Validator {
         }
 
         return true;
+    }
+
+
+    private boolean isHexa(String number) {
+        if ( number.length() == 0 ||
+                (number.charAt(0) != '-' && Character.digit(number.charAt(0), 16) == -1))
+            return false;
+        if ( number.length() == 1 && number.charAt(0) == '-' )
+            return false;
+
+        for ( int i = 1 ; i < number.length() ; i++ )
+            if ( Character.digit(number.charAt(i), 16) == -1 )
+                return false;
+        return true;
+    }
+
+    private Integer convertBase16(String number) throws ConversionException {
+
+        if(isHexa(number)){
+            return Integer.parseInt(number, 16) ;
+        }
+        else{
+            throw new ConversionException("The string can't be converted to base 16!");
+        }
+
     }
 }
